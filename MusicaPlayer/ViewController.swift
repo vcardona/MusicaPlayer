6920dc5cf01c89d3597e7b6c5b943ea6e29524d5
Victor Cardona <victorhugocardonagomez@gmail.com>
2019-05-03 16:38:20 -0500

    Initial Commit

---
+++ MusicaPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicaPlayer
+//
+//  Created by Victor Hugo on 5/3/19.
+//  Copyright © 2019 Vintage Robot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
